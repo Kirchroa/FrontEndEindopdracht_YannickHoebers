9dcaa60b4b75410d7855a4b2c98b89df69f725a6
Kirchroa <y.hoebers@novi-education.nl>
2021-05-14 19:22:56 +0200

    Add different way to view to show JSX in select element

--- src/components/SelectBar.js
+++ src/components/SelectBar.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const SelectBarTest = () => {
+const SelectBar = () => {
     
     //maak voor iedere keuze een apparte usestate aan
     const [vegetable, setVegetable] = useState([]);
@@ -51,13 +51,21 @@ const SelectBarTest = () => {
 
             const otherString = JSON.stringify(otherJson)
             const otherFormatted = otherString.replace(/[\[\]\"\']+/g,'')
-            console.log(otherFormatted)
             const otherArray = otherFormatted.split(",");
+
+            /////////////////////////////////////////////////////////////////////////////////////
+
+            const newData = vegetable.map((object) =>{
+                return object.name
+            })
+            console.log(newData)
             
+            
+    
+
+
+            ////////////////////////////////////////////////////////////////////////////////////
         
-            
-
-
             return (
                 <div>
                <select>
@@ -80,58 +88,19 @@ const SelectBarTest = () => {
         }
 
 
+
         return (
         <div>
          {getSelectList(vegetable, meat, other)}
         </div>
     )
+
+    
 }
 
-export default SelectBarTest
 
-/*
-const testIf = (response) => {
-            if (response = vegetable) {
-                const vegetableJson = vegetable.map((outer) => {
-                    return outer.recipes.map((inner) => {
-                        return inner.vegetable;
-                    })
-                });
-                const vegetableString = JSON.stringify(vegetableJson)
-                const vegetableFormatted = vegetableString.replace(/[\[\]\"\']+/g,'')
-                const vegetableArray = vegetableFormatted.split(",");
-    
-                return (
-                    <div>
-                   <select>
-                       {vegetableArray.map((item) => 
-                       <option>{item}</option>
-                       )}
-                   </select>
-                </div>
-                ) 
-            } else if (response = meat) {
-                
-                const meatJson = meat.map((outer) => {
-                    return outer.recipes.map((inner) => {
-                        return inner.meat;
-                    })
-                });
-                const meatString = JSON.stringify(meatJson)
-                const meatFormatted = meatString.replace(/[\[\]\"\']+/g,'')
-                const meatArray = meatFormatted.split(",");
-    
-                return (
-                    <div>
-                   <select>
-                       {meatArray.map((item) => 
-                       <option>{item}</option>
-                       )}
-                   </select>
-                </div>
-                ) 
 
-            }
-        }
-    
-*/
+
+
+
+export default SelectBar
